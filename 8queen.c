34b62775e28e1d25386c081ea91ac6eb71e98225
linuxluvsme <linuxluvsme@gmail.com>
2014-05-18 16:31:29 +0530

    update

--- 8queen.c
+++ 8queen.c
@@ -1,1 +1,15 @@
+//8 queen problem
 #include <stdio.h>
+
+int main()
+{
+	int queen[8][8]={0};
+	int i,j;
+	for(i=0;i<8;i++){
+		for(j=0;j<8;j++)
+			printf("%d ",queen[i][j]);
+		printf("\n");
+	}
+			
+	
+}
